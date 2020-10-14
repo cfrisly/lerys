6b0e46bcef0df0cc429456f6fd47b477e0d2da0e
Frisly <cfrisly@gmail.com>
2020-10-14 10:59:08 -0600

    Inicio

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Esto es una prueba de como se ve la primera pagina</p>
+</body>
+</html>
